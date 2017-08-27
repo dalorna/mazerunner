af1f139aa9ffc97d3609cf2e867d1cb56b115b29
dalorna <kcrango@hotmail.com>
2017-08-27 18:53:30 -0500

    server js with ..dist

---
+++ server/routes/api.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jason on 8/27/2017.
+ */
+const express = require('express');
+const router = express.Router();
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+module.exports = router;
